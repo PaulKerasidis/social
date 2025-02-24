7c585c3e4ca891743ca9e2fd2bbae14318f8bde5
Pkerasidis <p.kerasidis@icloud.com>
2025-02-24 17:46:54 +0200

    response

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"net/http"
-	"strconv"
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(strconv.Itoa(200)))
+	w.Write([]byte("OK"))
 }
