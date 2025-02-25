8d471f8ab0360418b092c1912baa7e9fccfac019
Pkerasidis <p.kerasidis@icloud.com>
2025-02-25 22:42:22 +0200

    implement go-chi library

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,34 +4,38 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
-
 	config config
-
 }
 
 type config struct {
 	addr string
-
 }
 
-func (app *application) mount() *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
-	return mux
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+
+	return r
 }
 
-func (app *application) run(mux *http.ServeMux) error {
+func (app *application) run(mux http.Handler) error {
 
 	srv := &http.Server{
-		Addr: app.config.addr,
-		Handler: mux,
+		Addr:         app.config.addr,
+		Handler:      mux,
 		WriteTimeout: time.Second * 30,
-		ReadTimeout: time.Second * 10,
-		IdleTimeout: time.Second * 60,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Second * 60,
 	}
 	log.Printf("Starting server on http://localhost%s", srv.Addr)
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/PaulKerasidis/social
 
 go 1.24.0
 
-require github.com/mattn/go-sqlite3 v1.14.24 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+)
